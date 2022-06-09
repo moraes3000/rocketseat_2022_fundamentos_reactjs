21082ccb0643d5954c03d5e8ec9d3a5243b961df
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-09 10:50:05 -0300

    tipagem

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+interface CommentProps {
+  content: string
+}
+
+export function Comment({ content }: CommentProps) {
   return (
     <div className={styles.comment}>
       <Avatar
